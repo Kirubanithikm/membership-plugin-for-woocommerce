686197024732f645dd065f866db0040aaf91a371
Kirubanithikm <kirubanithikm@gmail.com>
2024-01-24 09:49:38 +0530

    - add enqueue scripts

--- app/Route.php
+++ app/Route.php
@@ -24,10 +24,12 @@ class Route
         if(is_admin()) {
             add_action('admin_menu', array(self::$admin, 'membershipMenu'));
             add_filter('plugin_action_links_' . MPW_PLUGIN_BASENAME, array(self::$admin, 'mpwSettingsLink'));
+            add_action('admin_enqueue_scripts', array(self::$admin, 'loadAdminSettingsAssets'));
         }
 
         //Frontend
         add_action('woocommerce_before_add_to_cart_form', array(self::$product_page, 'membershipBanner'), 10);
         add_action('woocommerce_before_calculate_totals', array(self::$product_page, 'changePriceInCartForMembershipUser'), 10, 1);
+        add_action('wp_enqueue_scripts', array(self::$product_page, 'loadProductPageAssets'));
     }
 }
--- app/Views/Frontend/MembershipBanner.php
+++ app/Views/Frontend/MembershipBanner.php
@@ -2,17 +2,7 @@
 
 <?php if (!isset($discounted_price) || !isset($product_id) || !isset($discount_price) || !isset($original_price) || !isset($banner_message) || !isset($banner_saved_message)) exit; ?>
 
-<style>
-    .product-banner {
-        background-color: gold;
-        padding: 20px 20px;
-        text-align: center;
-        font-size: 18px;
-        color: #333;
-    }
-</style>
-
-<div class="product-banner">
+<div class="mpw-membership-banner">
     <p><?php esc_html_e($banner_message, 'membership-plugin-woocommerce'); ?> <?php echo '<del>' . wc_price($original_price) . '</del>' . '&nbsp' . '<ins>' . wc_price($discounted_price) . '</ins>'; ?></p>
     <p><?php esc_html_e($banner_saved_message, 'membership-plugin-woocommerce'); ?> <?php echo wc_price($discount_price); ?></p>
 </div>
